bf8524e9a84a62c98f6eb0fc352cda48c8b4d769
Kevin Calaway <calaway@usc.edu>
2018-01-12 12:25:46 -0800

    Better error checking!

--- gitpractice.cpp
+++ gitpractice.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-	cout << "Please enter an integer." << endl;
 	bool checker = true;
 	int i;
 	while(checker){
+		cout << "Please enter an integer." << endl;
 		cin >> i;
 		if (cin.fail()){
 			cout << "That's not an integer! Try again." << endl;
+			cin.clear();
+			string garbage;
+			cin >> garbage;
 		}
 		else {
 			checker = false;
